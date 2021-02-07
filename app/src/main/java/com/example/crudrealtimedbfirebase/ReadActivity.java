afa22c2a3fc084cdd4f9f0b58b330ef8c3440259
Maulidani <maulidanimahmud@gmail.com>
2021-02-07 23:11:14 +0800

    update

--- app/src/main/java/com/example/crudrealtimedbfirebase/ReadActivity.java
+++ app/src/main/java/com/example/crudrealtimedbfirebase/ReadActivity.java
@@ -61,22 +61,7 @@ public class ReadActivity extends AppCompatActivity {
                 adapter = new rvAdapter(listReq, ReadActivity.this);
                 rv.setAdapter(adapter);
                 loading.dismiss();
-//                        // Read from the database
-//                myRef.addValueEventListener(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(DataSnapshot dataSnapshot) {
-//                        // This method is called once with the initial value and again
-//                        // whenever data at this location is updated.
-//                        String value = dataSnapshot.getValue(String.class);
-//                        Log.d(TAG, "Value is: " + value);
-//                    }
-//
-//                    @Override
-//                    public void onCancelled(DatabaseError error) {
-//                        // Failed to read value
-//                        Log.w(TAG, "Failed to read value.", error.toException());
-//                    }
-//                });
+
             }
 
             @Override
